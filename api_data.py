30e4ed33fc6a7255b69360d628062597143f841f
gagandeep singh <69230886+GaganBTS@users.noreply.github.com>
2021-08-04 12:05:20 +0530

    Create api_data.py

---
+++ api_data.py
@@ -0,0 +1,13 @@
+import requests as api
+api_key ="YOUR_API_KEY"
+class Weather_Data():
+    def __init__(self,city,unit):
+        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={unit}"
+        self.response = api.get(self.url)
+        self.weather = self.response.json()
+    def temp(self):
+        self.temperature = self.weather["main"]["temp"]
+        return self.temperature
+    def atmosphere(self):
+        self.atmos = self.weather["weather"][0]["description"]
+        return self.atmos
